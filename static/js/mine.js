a960241a6dbf0e372a8d56a688554e9b6e3d37f6
TracyWei <tracy_wei7@163.com>
2015-09-03 16:49:07 +0800

    主页ok

--- static/js/mine.js
+++ static/js/mine.js
@@ -1,14 +1,5 @@
     $(function(){
-        $('.banner').unslider({arrows: true,
-            delay:1000000,
-            keys: true,
-            fluid: true,
-            dots: true
-        });
-        $('.unslider-arrow').click(function() {
-        var fn = this.className.split(' ')[1];
-
-        //  Either do unslider.data('unslider').next() or .prev() depending on the className
-        unslider.data('unslider')[fn]();
-        });
+        $('.carousel').carousel({
+    interval: 20000000
+    })
     });
