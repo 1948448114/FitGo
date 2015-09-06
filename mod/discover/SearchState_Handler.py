cbf447e86fb030c9c4be0ac422efc6c05bf9d283
yue-b <yue@byuedeMacBook-Pro.local>
2015-09-06 16:28:15 +0800

    createstateOK

--- mod/discover/SearchState_Handler.py
+++ mod/discover/SearchState_Handler.py
@@ -5,6 +5,7 @@ import tornado.web
 import tornado.gen
 from mod.auth.Base_Handler import BaseHandler
 from ..databases.tables import TopicsCache
+# from sqlalchemy import func
 
 #/discover/search/state
 class SearchStateHandler(BaseHandler):
@@ -13,9 +14,20 @@ class SearchStateHandler(BaseHandler):
 
 		try:
 			topics = self.db.query(TopicsCache).filter(TopicsCache.topic_title==a_topic_title).all()
-			retjson = {'code':400,'content':''}
+			# topics_count = self.db.query(func.count(topics))
+			print topics_count,topics[0].uid,topics[1].uid
+			print "======"
+			# retjson = {'code':400,'content':topics[0].uid}
+			retjson = {'code':400,'content':''}#{'uid':'','topic_id':''}
+			# for n in topics:
+			# 	print n
+			retjson.content.push({'uid':topics[0].uid,"topic_id":topics[0].topic_id})
+
+			self.write(retjson)
+
 		except Exception,e:
 			print e
 			retjson = {'code':400,'content':'failed to search'}
+			self.write(retjson)
 
 
