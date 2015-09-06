b9b8f585f8c3cb684abe3bc45539e1cce09d2707
yue-b <yue@byuedeMacBook-Pro.local>
2015-09-06 09:06:05 +0800

    create_state

--- mod/discover/CreateState_Handler.py
+++ mod/discover/CreateState_Handler.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 
-#import tornado.web
-#import tornado.gen
+import tornado.web
+import tornado.gen
+import json
+from ..databases.tables import TopicsCache
+from mod.auth.Base_Handler import BaseHandler
+
 #/discover/create
-class CreateStateHandler(tornado.web.RequestHandler):
+class CreateStateHandler(BaseHandler):
 	def post(self):#发布动态
-		pass
-		#a_time = self.get_argument("time")
-        #a_topics = self.get_argument("topics")
-        #a_state_content = self.get_argument("state_content")
-        #self.render("discoverpage.html",a_time=time,a_topics=topics,a_state_content=state_content)
+		user_id = self.get_argument('uid')
+		a_topic_title = self.get_argument('topic_title')
+		a_topic_content = self.get_argument('topic_content')
+		a_topic_pic = self.get_argument('topic_pic')
+		topics = TopicsCache(uid=user_id,topic_title=a_topic_title,topic_content=a_topic_content,topic_pic=a_topic_pic)
+		self.db.add(topics)
