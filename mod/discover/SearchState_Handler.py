126aabd95d6e4e7f7a3dfac4e55df7664f10f3da
yue-b <yue@byuedeMacBook-Pro.local>
2015-09-08 08:35:24 +0800

    searchstate修改

--- mod/discover/SearchState_Handler.py
+++ mod/discover/SearchState_Handler.py
@@ -11,23 +11,38 @@ from ..databases.tables import TopicsCache
 class SearchStateHandler(BaseHandler):
 	def post(self):#搜索动态
 		a_topic_title = self.get_argument('topic_title')
-
-		try:
-			topics = self.db.query(TopicsCache).filter(TopicsCache.topic_title==a_topic_title).all()
-			retjson = {'code':200,'content':'ok'}
-			content1 = []
-			for n in topics:
-				content = {}
-				content['uid'] = n.uid
-				content['topic_id'] = n.topic_id
-				content1.append(content)
-			retjson['content'] = content1
-			print retjson
-			self.write(retjson)
-
-		except Exception,e:
-			print e
-			retjson = {'code':400,'content':'failed to search state'}
+		if a_topic_title:
+			try:
+				# s=self.db.execute('select * from Users;').first()
+				# print s
+				topics = self.db.execute('select * from Topics where topic_title like '%'a_topic_title'%';').all()
+				# topics = self.db.query(TopicsCache).filter(TopicsCache.topic_title.like(%a_topic_title%)).all()
+				print topics
+				
+				if topics:
+					retjson = {'code':200,'content':'ok'}
+					content1 = []
+					for n in topics:
+						content = {}
+						content['uid'] = n.uid
+						content['topic_id'] = n.topic_id
+						content['topic_title'] = n.topic_title
+						content['topic_content'] = n.topic_content
+						content['topic_pic'] = n.topic_pic
+						content['topic_starers'] = n.topic_starers
+						content1.append(content)
+					retjson['content'] = content1
+					print retjson
+					self.write(retjson)
+				else:
+					retjson = {'code':400,'content':'not match topics_title'}
+					self.write(retjson)
+			except Exception,e:
+				print e
+				retjson = {'code':400,'content':'failed to search state'}
+				self.write(retjson)
+		else:
+			retjson = {'code':400,'content':'topic_title is null'}
 			self.write(retjson)
 
 
