682da4931afd5b4e311581517ea7b4d77adf3d62
AnandrewChan <andrew_rong@163.com>
2015-09-08 14:13:46 +0800

    日历被抛弃了，时间轴来啦

--- mod/discover/SearchState_Handler.py
+++ mod/discover/SearchState_Handler.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 
@@ -45,40 +44,3 @@ class SearchStateHandler(BaseHandler):
 		else:
 			retjson = {'code':400,'content':'topic_title is null'}
 			self.write(retjson)
-
-
-=======
-# -*- coding: utf-8 -*-
-#!/usr/bin/env python
-
-import tornado.web
-import tornado.gen
-from mod.auth.Base_Handler import BaseHandler
-from ..databases.tables import TopicsCache
-# from sqlalchemy import func
-
-#/discover/search/state
-class SearchStateHandler(BaseHandler):
-	def post(self):#搜索动态
-		a_topic_title = self.get_argument('topic_title')
-
-		try:
-			topics = self.db.query(TopicsCache).filter(TopicsCache.topic_title==a_topic_title).all()
-			retjson = {'code':400,'content':'ok'}
-			content1 = []
-			for n in topics:
-				content = {}
-				content['uid'] = n.uid
-				content['topic_id'] = n.topic_id
-				content1.append(content)
-			retjson['content'] = content1
-			print retjson
-			self.write(retjson)
-
-		except Exception,e:
-			print e
-			retjson = {'code':400,'content':'failed to search state'}
-			self.write(retjson)
-
-
->>>>>>> origin/master
