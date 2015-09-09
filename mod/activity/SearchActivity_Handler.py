04745093fca256b01a5a2a2ee1b5ed390ddcdce8
yue-b <yue@byuedeMacBook-Pro.local>
2015-09-09 11:36:40 +0800

    search_friend/activity(有问题）

--- mod/activity/SearchActivity_Handler.py
+++ mod/activity/SearchActivity_Handler.py
@@ -8,31 +8,41 @@ from ..databases.tables import ActCache
 #/activity/search/
 class SearchActivityHandler(BaseHandler):
 	def post(self):#搜索活动
-		user_id = self.get_argument("uid")
 		a_act_title = self.get_argument("act_title")
 		a_start_time = self.get_argument("start_time")
-		a_end_time = self.get_argument("end_time")
 		a_location = self.get_argument("location")
+		string = ''
 
 		try:
-			activitys = self.db.query(ActCache).filter(ActCache.act_title==a_act_title).all()
-			retjson = {'code':200,'content':'ok'}
-			content1 = []
-			for n in activitys:
-				content = {}
-				content['uid'] = n.uid
-				content['act_id'] = n.act_id
-				content['act_title'] = n.act_title
-				content['start_time'] = n.start_time
-				content['end_time'] = n.end_time
-				content['act_location'] = n.location
-				content['details'] = n.details
-				content['join_people'] = n.join_people
-				content1.append(content)
-			retjson['content'] = content1
-			print retjson
-			self.write(retjson)
-		except Exception,e:
-			print e
+			if a_act_title:
+				string = string + 'act_title like \'%%%s%%\'' % a_act_title
+			if a_start_time:
+				string = string + 'start_time=\'%s\'' % a_start_time
+			if a_location:
+				string = string + 'user_name=\'%s\'' % a_username
+
+			if string.strip()=='':
+				retjson = {'code':400,'content':'all parameters are null'}
+			else:
+				activitys = self.db.execute("select * from Act where %s;" % string).fetchall()
+				if activitys:
+					retjson = {'code':200,'content':'ok'}
+					content1 = []
+					for n in activitys:
+						content = {}
+						content['uid'] = n.uid
+						content['act_id'] = n.act_id
+						content['act_title'] = n.act_title
+						content['start_time'] = n.start_time
+						content['end_time'] = n.end_time
+						content['act_location'] = n.location
+						content['details'] = n.details
+						content['join_people'] = n.join_people
+						content1.append(content)
+					retjson['content'] = content1
+					print retjson
+				else:
+					retjson = {'code':400,'content':'not match activity'}
+		except Exception, e:
 			retjson = {'code':400,'content':'failed to search activity'}
-			self.write(retjson)
+		self.write(retjson)
--- mod/discover/SearchFriend_Handler.py
+++ mod/discover/SearchFriend_Handler.py
@@ -19,10 +19,40 @@ class SearchFriendHandler(BaseHandler):
 		a_user_score = self.get_argument('user_score')
 		a_user_join_event = self.get_argument('user_join_event')
 
-		print a_username
-		retjson = {'code':200,'content':'ok'}
+		string = ''
 		try:
-			person1 = self.db.query(UsersCache).filter(UsersCache.user_name==a_username)
+			if a_username:
+				string = string + 'user_name=\'%s\'' % a_username + ' and '
+			if a_name:
+				string = string + 'name=\'%s\'' % a_name + ' and '
+			if a_campus:
+				string = string + 'campus=\'%s\'' % a_campus + ' and '
+			if a_school:
+				string = string + 'school=\'%s\'' % a_school + ' and '
+			if a_user_enjoyment:
+				stirng = string + 'user_enjoyment like \'%%%s%%\'' % a_user_enjoyment + ' and '
+			if a_user_join_times:
+				string = string + 'user_join_times=\'%s\'' % a_user_join_times + ' and '
+			if a_user_join_event:
+				string = string + 'user_join_event=\'%s\'' % a_user_join_event  + ' and '
+			if a_user_score:
+				string = string + 'user_score=\'%s\'' % a_user_score
+			print string
+			print "select * from Users where %s;" % string
+			if string.strip()=='':
+				retjson = {'code':400,'content':'all parameters are null'}
+			else:
+				persons = self.db.execute("select * from Users where %s;" % string).fetchall()
+				if persons:
+					retjson = {'code':200,'content':'ok'}
+					content1 = []
+					for n in persons:
+						content = {}
+						content['uid'] = n.uid
+						content1.append(content)
+					retjson['content'] = content1
+				else:
+					retjson = {'code':400,'content':'not match friend'}	
 
 
 		except Exception,e:
