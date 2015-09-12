e44fd291b407e538953048bddbca9a1e37ced63e
LJ <1948448114@qq.com>
2015-09-12 17:02:38 +0800

    discover改完一点

--- mod/discover/DiscoverPage_Handler.py
+++ mod/discover/DiscoverPage_Handler.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 
@@ -6,119 +5,40 @@ import tornado.web
 import tornado.gen
 from mod.auth.Base_Handler import BaseHandler
 from ..databases.tables import TopicsCache
+import traceback
 
 #/discover/discover_page
 class DiscoverPageHandler(BaseHandler):
-	def get(self):#发现主页面
-		content=[	
-					{'square':'',
-					'state_img':'1',
-			       'id':'1',
-                   'state_title':'lala',
-        			'state_writer_link':'/plans',
-        			'state_writer':'Andrew',
-        			'state_detail':"lalalalal"},
-        			{'square':'',
-					'state_img':'2',
-			       'id':'2',
-                   'state_title':'lala',
-        			'state_writer_link':'/plans',
-        			'state_writer':'Andrew',
-        			'state_detail':"lalalalal"},
-        			{'square':'',
-					'state_img':'3',
-			       'id':'3',
-                   'state_title':'lala',
-        			'state_writer_link':'/plans',
-        			'state_writer':'Andrew',
-        			'state_detail':"lalalalal"},
-        			{'square':'',
-					'state_img':'4',
-			       'id':'4',
-                   'state_title':'lala',
-        			'state_writer_link':'/plans',
-        			'state_writer':'Andrew',
-        			'state_detail':"lalalalal"},
-        			{'square':'uc-container_square',
-					'state_img':'5',
-			       'id':'5',
-                   'state_title':'lala',
-        			'state_writer_link':'/plans',
-        			'state_writer':'Andrew',
-        			'state_detail':"lalalalal"},
-        			{'square':'uc-container_square',
-					'state_img':'6',
-			       'id':'6',
-                   'state_title':'lala',
-        			'state_writer_link':'/plans',
-        			'state_writer':'Andrew',
-        			'state_detail':"lalalalal"},
-        			{'square':'uc-container_square',
-					'state_img':'7',
-			       'id':'7',
-                   'state_title':'lala',
-        			'state_writer_link':'/plans',
-        			'state_writer':'Andrew',
-        			'state_detail':"lalalalal"},
-        			{'square':'uc-container_square',
-					'state_img':'8',
-			       'id':'8',
-                   'state_title':'lala',
-        			'state_writer_link':'/plans',
-        			'state_writer':'Andrew',
-        			'state_detail':"lalalalal"},
-        			{'square':'',
-					'state_img':'9',
-			       'id':'9',
-                   'state_title':'lala',
-        			'state_writer_link':'/plans',
-        			'state_writer':'Andrew',
-        			'state_detail':"lalalalal"},
-        			
-        			{'square':'',
-					'state_img':'10',
-			       'id':'10',
-                   'state_title':'lala',
-        			'state_writer_link':'/plans',
-        			'state_writer':'Andrew',
-        			'state_detail':"lalalalal"},
-        			{'square':'',
-					'state_img':'11',
-			       'id':'11',
-                   'state_title':'lala',
-        			'state_writer_link':'/plans',
-        			'state_writer':'Andrew',
-        			'state_detail':"lalalalal"},
-        			{'square':'',
-					'state_img':'12',
-			       'id':'12',
-                   'state_title':'lala',
-        			'state_writer_link':'/plans',
-        			'state_writer':'Andrew',
-        			'state_detail':"lalalalal"}
-        ]
-		self.render('discoverpage.html',user=self.current_user,content=content)
-	def post(self):
-		times = self.get_argument("times")#刷新次数［0,1，2，。。。。］
+    def get(self):#发现主页面
+        self.render('discoverpage.html',user=self.current_user)
+    def post(self):
         try:
-            topics = self.db.query(TopicsCache).order_by(TopicsCache.topic_time.desc())[12*times:12*times+11]
-            if topics:
-                retjson = {'code':200,'content':'success to query state'}
-                content1 = []
-                for n in topics:
-                    content = {}
-                    content['uid'] = n.uid
-                    content['topics_id'] = n.topics_id
-                    content['topic_time'] = n.topic_time
-                    content['topic_content'] = n.topic_content
-                    content['topic_pic '] = n.topic_pic
-                    content['pic_shape'] = n.pic_shape
-                    content['topic_title '] = n.topic_title
-                    content['topic_starers'] = n.topic_starers 
-                    content1.append(content)
-                retjson['content'] = content1
-            else:
-                retjson = {'code':400,'content':'have no state'}
+            times = self.get_argument("times")
+            print times
+            try:
+                topics = self.db.query(TopicsCache).order_by(TopicsCache.topic_time.desc())
+                if topics:
+                    retjson = {'code':200,'content':'success to query state'}
+                    content1 = []
+                    for n in topics:
+                        content = {}
+                        content['uid'] = n.uid
+                        content['topics_id'] = n.topic_id
+                        content['topic_time'] = n.topic_time
+                        content['topic_content'] = n.topic_content
+                        content['topic_pic'] = n.topic_pic
+                        content['pic_shape'] = n.pic_shape
+                        content['topic_title'] = n.topic_title
+                        content['topic_starers'] = n.topic_starers 
+                        content1.append(content)
+                    retjson['content'] = content1
+                else:
+                    retjson = {'code':400,'content':'have no state'}
+            except Exception,e:
+                print traceback.print_exc()
+                print str(e)
+                retjson = {'code':400,'content':'failed to query state'}
+            self.render('discover_state.html',content=retjson)
         except Exception,e:
-            print e
-            retjson = {'code':400,'content':'failed to query state'}
+            print traceback.print_exc()
+            print str(e)
--- static/js/discoverController.js
+++ static/js/discoverController.js
@@ -1,1 +1,278 @@
-discoverController.js
+var times=0;
+//加载的js
+$(document).ready(function() {
+    init();
+
+
+    
+    
+
+
+
+    getAllState();
+    
+});
+
+//初始化函数
+function init(){
+
+}
+
+function getAllState(){
+    jQuery.ajax({
+      url: '/discover/discover_page',
+      type: 'POST',
+      data: {
+        'times': times},
+      success: function(data, textStatus, xhr) {
+        console.log("two");
+        $("#discover_state_all").html(data);
+
+        other();
+        friendtState();
+        AllStatezhan();
+        times=times+1;
+
+      },
+      error: function(xhr, textStatus, errorThrown) {
+        //called when there is an error
+      }
+    });
+};
+
+function AllStatezhan() {
+    var opened = false;
+    $('#discover_state_all > div.uc-container').each(function(i) {
+        var $item = $(this),
+            direction;
+        switch (i % 12) {
+            case 0:
+                direction = ['right', 'bottom'];
+                break;
+            case 1:
+                direction = ['right', 'bottom'];
+                break;
+            case 2:
+                direction = ['left', 'bottom'];
+                break;
+            case 3:
+                direction = ['left', 'bottom'];
+                break;
+            case 4:
+                direction = ['top', 'right'];
+                break;
+            case 5:
+                direction = ['bottom', 'right'];
+                break;
+            case 6:
+                direction = ['top', 'left'];
+                break;
+            case 7:
+                direction = ['bottom', 'left'];
+                break;
+            case 8:
+                direction = ['right', 'top'];
+                break;
+            case 9:
+                direction = ['right', 'top'];
+                break;
+            case 10:
+                direction = ['left', 'top'];
+                break;
+            case 11:
+                direction = ['left', 'top'];
+                break;
+        }
+        var pfold = $item.pfold({
+            folddirection: direction,
+            speed: 200,
+            onEndFolding: function() {
+                opened = false;
+            },
+            centered: true
+        });
+        $item.find('span.icon-eye').on('click', function() {
+            if (!opened) {
+                opened = true;
+                pfold.unfold();
+            }
+        }).end().find('span.icon-cancel').on('click', function() {
+            pfold.fold();
+        });
+    });
+};
+
+function friendtState() {
+    // say we want to have only one item opened at one moment
+    var opened = false;
+    $('#discover_state_friend > div.uc-container').each(function(i) {
+        var $item1 = $(this),
+            direction;
+        switch (i % 8) {
+            case 0:
+                direction = ['right', 'bottom'];
+                break;
+            case 1:
+                direction = ['right', 'bottom'];
+                break;
+            case 2:
+                direction = ['left', 'bottom'];
+                break;
+            case 3:
+                direction = ['left', 'bottom'];
+                break;
+            case 4:
+                direction = ['right', 'top'];
+                break;
+            case 5:
+                direction = ['right', 'top'];
+                break;
+            case 6:
+                direction = ['left', 'top'];
+                break;
+            case 7:
+                direction = ['left', 'top'];
+                break;
+        }
+        var pfold = $item1.pfold({
+            folddirection: direction,
+            speed: 300,
+            onEndFolding: function() {
+                opened = false;
+            },
+            // centered : true
+        });
+        $item1.find('span.icon-eye').on('click', function() {
+            if (!opened) {
+                opened = true;
+                pfold.unfold();
+            }
+        }).end().find('span.icon-cancel').on('click', function() {
+            pfold.fold();
+        });
+    });
+};
+function other() {
+    // say we want to have only one item opened at one moment
+    var opened = false;
+    $('#discover_state_list > div.uc-container').each(function(i) {
+        var $item1 = $(this),
+            direction;
+        switch (i % 4) {
+            case 0:
+                direction = ['right', 'bottom'];
+                break;
+            case 1:
+                direction = ['right', 'bottom'];
+                break;
+            case 2:
+                direction = ['left', 'bottom'];
+                break;
+            case 3:
+                direction = ['left', 'bottom'];
+                break;
+        }
+        var pfold = $item1.pfold({
+            folddirection: direction,
+            speed: 600,
+            onEndFolding: function() {
+                opened = false;
+            },
+            // centered : true
+        });
+        $item1.find('span.icon-eye').on('click', function() {
+            if (!opened) {
+                opened = true;
+                pfold.unfold();
+            }
+        }).end().find('span.icon-cancel').on('click', function() {
+            pfold.fold();
+        });
+    });
+};
+$(function() {
+
+    var a = new sHover("friend_item", "friend_cover");
+    a.set({
+        slideSpeed: 5,
+        opacityChange: true,
+        opacity: 80
+    });
+});
+$(function() {
+    $(".icon-camera").click(function(event) {
+        /* Act on the event */
+        $("#warp").show('slow/400/fast');
+        $("#discover_png").hide('slow/400/fast');
+        $(".container_friend").hide();
+        $("#friend_state").hide();
+        $("#search_state_list").hide();
+        $(".find_friend").hide();
+    });
+
+
+    $("#share_btn").click(function(event) {
+        /* Act on the event */
+        $(".create_state").show('slow/400/fast');
+        $(".find_friend").hide('slow/400/fast');
+        $(".container_friend").hide();
+        $("#friend_state").hide();
+        $("#search_state_list").hide();
+    });
+
+
+    $("#find_friend_btn").click(function(event) {
+        /* Act on the event */
+        $(".create_state").hide();
+        $(".find_friend").hide();
+        $(".container_friend").show();
+        $("#search_state_list").hide();
+        $("#friend_state").hide();
+    });
+
+
+    $("#submit_state_btn").click(function(event) {
+        /* Act on the event */
+        $(".create_state").hide();
+        $(".find_friend").hide();
+        $("#friend_state").show();
+        $(".container_friend").hide();
+        $("#search_state_list").hide();
+        window.location.reload();
+    });
+
+
+    $("#find_btn").click(function(event) {
+        /* Act on the event */
+        $(".find_friend").show('slow/400/fast');
+        $(".create_state").hide('slow/400/fast');
+        $("#friend_state").hide();
+        $("#search_state_list").hide();
+        $(".create_state").hide();
+    });
+
+
+    $("#search_state_btn").click(function(event) {
+        /* Act on the event */
+        $("#search_state_list").show();
+        $(".create_state").hide();
+        $(".container_friend").hide();
+        $(".find_friend").hide();
+        $("#friend_state").hide();
+    });
+
+
+
+    $(".icon-spinner").click(function(event) {
+        /* Act on the event */
+        console.log('here');
+        getAllState();
+        // window.location.reload();
+    });
+});
+$(function() {
+    $("#menu .home").removeClass("home");
+    $($("#menu .bar").get(1)).addClass("home");
+});
+
+
