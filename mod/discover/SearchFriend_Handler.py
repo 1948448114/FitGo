365a515edfcbf03c34e405be654daf1239c7e6ad
yue-b <yue@byuedeMacBook-Pro.local>
2015-09-12 11:11:47 +0800

    linushi

--- mod/discover/SearchFriend_Handler.py
+++ mod/discover/SearchFriend_Handler.py
@@ -9,34 +9,24 @@ from ..databases.tables import UsersCache,User_tagCache
 #/discover/search/friends
 class SearchFriendHandler(BaseHandler):
 	def post(self):#搜索好友
-		a_username = self.get_argument('user_name')
 		a_name = self.get_argument('name')
+		a_gender = self.get_argument('gender')
 		a_campus = self.get_argument('campus')
 		a_school = self.get_argument('school')
-		#user_tag
 		a_user_enjoyment = self.get_argument('user_enjoyment')
-		a_user_join_times = self.get_argument('user_join_times')
-		a_user_score = self.get_argument('user_score')
-		a_user_join_event = self.get_argument('user_join_event')
 
 		string = ''
 		try:
-			if a_username:
-				string = string + 'user_name=\'%s\'' % a_username + ' and '
 			if a_name:
-				string = string + 'name=\'%s\'' % a_name + ' and '
+				string = string + 'name like \'%%%s%%\'' % a_name + ' and '
 			if a_campus:
 				string = string + 'campus=\'%s\'' % a_campus + ' and '
 			if a_school:
 				string = string + 'school=\'%s\'' % a_school + ' and '
+			if a_gender:
+				string = string + 'gender=\'%s\'' % a_gender + ' and '
 			if a_user_enjoyment:
 				stirng = string + 'user_enjoyment like \'%%%s%%\'' % a_user_enjoyment + ' and '
-			if a_user_join_times:
-				string = string + 'user_join_times=\'%s\'' % a_user_join_times + ' and '
-			if a_user_join_event:
-				string = string + 'user_join_event=\'%s\'' % a_user_join_event  + ' and '
-			if a_user_score:
-				string = string + 'user_score=\'%s\'' % a_user_score + ' and '
 
 
 			print "select * from Users where %s;" % string
@@ -54,6 +44,9 @@ class SearchFriendHandler(BaseHandler):
 					for n in persons:
 						content = {}
 						content['uid'] = n.uid
+						content['name'] = n.name
+						content['signature'] = n.name
+						content['portrait'] = n.portrait
 						content1.append(content)
 					retjson['content'] = content1
 				else:
