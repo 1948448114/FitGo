1eab8f38a9f1406fc1dd8a745c3b760ec2b4ff19
yue-b <yue@byuedeMacBook-Pro.local>
2015-09-17 02:19:39 +0800

    okok

--- mod/auth/Password_Handler.py
+++ mod/auth/Password_Handler.py
@@ -16,11 +16,11 @@ class PasswordHandler(BaseHandler):
 	def put(self):
 		old_passwd = self.get_argument('old_password')
 		new_passwd = self.get_argument('new_password')
-		# user_cookie = self.current_user
-		user_cookie = self.get_argument('uid')
+		user_cookie = self.current_user
+		# user_cookie = self.get_argument('uid')
 		retjson = {"code":200,"content":""}
 		try:
-			usr1=self.db.query(UsersCache).filter(UsersCache.uid==user_cookie).one()
+			usr1=self.db.query(UsersCache).filter(UsersCache.uid==user_cookie.uid).one()
 			old_salt = usr1.salt
 			old_password = hashlib.md5(old_salt.join(old_passwd)).hexdigest()
 			if old_password == usr1.password:
