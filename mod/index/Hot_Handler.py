aba07cd07249b482d1bf6dcafef44e81c61cc425
qinshimeng <1538160868@qq.com>
2015-09-17 09:50:21 +0800

    hot page

--- mod/index/Hot_Handler.py
+++ mod/index/Hot_Handler.py
@@ -5,46 +5,57 @@ import tornado.gen
 from ..auth.Base_Handler import BaseHandler
 from ..databases.tables import InviteCache,UsersCache
 import json,time,string
+import traceback
 
 class HotHandler(BaseHandler):
 	def get(self):
-		retjson = {'code':200,'content':'Null'}
-		contentall=[]
+		retjson = {'code':200,'content':{
+								'invite':[],
+								'act':[],
+								'topics':[]
+
+		}}
 		time_now=time.time()
-		sql_invite="select * from Invite where start_time >= %s order by start_time;" % str(time_now)
-		sql_act = "select *from Act where start_time >= %s order by start_time;" % str(time_now)
+		sql_invite = "select * from Invite where start_time >= %s order by start_time desc limit 2;" % str(time_now)
+		sql_act = "select *from Act where start_time >= %s order by start_time desc limit 1;" % str(time_now)
+		sql_topics = "select * from Topics order by topic_time desc limit 3;"
 		try:
-			invitations=self.db.execute(sql_invite).fetchall()[0:2]
+			contentall1=[]
+			invitations=self.db.execute(sql_invite).fetchall()
 			for i in invitations:
 				content={}
 				content['start_time'] = time.strftime("%Y-%m-%d %H:%M",time.localtime(int(i.start_time)))
 				content['fit_item'] = i.fit_item
 				content['user_tag'] = i.user_tag
 				content['remark'] = i.remark
-				contentall.append(content)
+				content['gender'] = i.gender
+				contentall1.append(content)
+			retjson['content']['invite']=contentall1
 
-			# activities=self.db.execute(sql_act).fetchall[0:1]
-			# for n in activities:
-			# 	content={}	
-			# 	content['title'] = n.act_title
-			# 	content['start'] = strftime("%Y-%m-%d",localtime(int(n.start_time)))
-			# 	content['detail'] = n.act_detail
-			# 	contentall.append(content)
+			contentall2=[]
+			activities=self.db.execute(sql_act).fetchall()
+			for n in activities:
+				content={}
+				content['title'] = n.act_title
+				content['start'] = time.strftime("%Y-%m-%d",time.localtime(int(n.start_time)))
+				content['detail'] = n.act_detail
+				content['act_location'] = n.act_location
+				contentall2.append(content)
+			retjson['content']['act']=contentall2
 
-			# state = self.Mongodb().Plan.find()
+			contentall3=[]
+			activities=self.db.execute(sql_topics).fetchall()
+			for n in activities:
+				content={}
+				content['topic_time'] = time.strftime("%Y-%m-%d",time.localtime(int(n.topic_time)))
+				content['topic_content'] = n.topic_content
+				content['topic_title'] = n.topic_title
+				contentall3.append(content)
+			retjson['content']['topics']=contentall3
 			
-			# for i in state:
-			# 	content={}
-			# 	content['target']=i.target
-			# 	content['signature']=i.signature
-			# 	content['start_time']=i.start_time
-			# 	content['end_time']=i.end_time
-			# 	contentall.append(content)
-
-			retjson['content'] = contentall
 		except Exception, e:
-			print e
 			retjson['code'] = 401
 			retjson['content'] = u'Nothing found!Please try other conditions'
+			
 		ret = json.dumps(retjson,ensure_ascii=False, indent=2)
 		self.write(ret)
